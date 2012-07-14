89de8d1383c89131b4eba04fcbc049a7b71ec3c1
chenli <lic9@uci.edu>
2012-07-14 01:54:47 -0700

    change to 2 spaces

--- Java/Point.java
+++ Java/Point.java
@@ -1,33 +1,33 @@
 public class Point{
-    private int x;
-    private int y;
+  private int x;
+  private int y;
     
-    public Point(int a, int b) {
-        x = a;
-        y = b;
-    }
+  public Point(int a, int b) {
+    x = a;
+    y = b;
+  }
     
-    public int getX() {
-        return x;
-    }
+  public int getX() {
+    return x;
+  }
     
-    public int getY() {
-        return y;
-    }
+  public int getY() {
+    return y;
+  }
     
-    public void increX() {
-        x = x + 1;
-    }
+  public void increX() {
+    x = x + 1;
+  }
     
-    public void decreX() {
-        x = x - 1;
-    }
+  public void decreX() {
+    x = x - 1;
+  }
     
-    public void increY() {
-        y = y + 1;
-    }
+  public void increY() {
+    y = y + 1;
+  }
     
-    public void decreY() {
-        y = y - 1;
-    }
+  public void decreY() {
+    y = y - 1;
+  }
 }
--- Java/Robot.java
+++ Java/Robot.java
@@ -1,70 +1,70 @@
 public class Robot{
-    private Point position;
-    private int lambdaCount;
-    private int score;
-    private int waterTime;
+  private Point position;
+  private int lambdaCount;
+  private int score;
+  private int waterTime;
     
-    public Robot(int x, int y) {
-        position = new Point(x, y);
-        lambdaCount = 0;
-        score = 0;
-        waterTime = 0;
-    }
+  public Robot(int x, int y) {
+    position = new Point(x, y);
+    lambdaCount = 0;
+    score = 0;
+    waterTime = 0;
+  }
     
-    public void moveUp() {
-        position.increY();
-        score--;
-    }
+  public void moveUp() {
+    position.increY();
+    score--;
+  }
     
-    public void moveDown() {
-        position.decreY();
-        score--;
-    }
+  public void moveDown() {
+    position.decreY();
+    score--;
+  }
     
-    public void moveLeft() {
-        position.decreX();
-        score--;
-    }
-    
-    public void moveRight() {
-        position.increX();
-        score--;
-    }
-    
-    public void stay() {
-        score--;
-    }
-    
-    public void gainLambda() {
-        lambdaCount++;
-        score += 25;
-    }
-    
-    public void liftLambda() {
-        score += 50*lambdaCount;
-    }
-    
-    public void abort() {
-        score += 25*lambdaCount;
-    }
-    
-    public int getScore() {
-        return score;
-    }
-    
-    public int getLambda() {
-        return lambdaCount;
-    }
-    
-    public void cleanWaterTime() {
-        waterTime = 0;
-    }
-    
-    public void stayInWater() {
-        waterTime++;
-    }
-    
-    public int getWaterTime() {
-        return waterTime;
-    }
+  public void moveLeft() {
+      position.decreX();
+      score--;
+  }
+  
+  public void moveRight() {
+      position.increX();
+      score--;
+  }
+  
+  public void stay() {
+      score--;
+  }
+  
+  public void gainLambda() {
+      lambdaCount++;
+      score += 25;
+  }
+  
+  public void liftLambda() {
+      score += 50*lambdaCount;
+  }
+  
+  public void abort() {
+      score += 25*lambdaCount;
+  }
+  
+  public int getScore() {
+      return score;
+  }
+  
+  public int getLambda() {
+      return lambdaCount;
+  }
+  
+  public void cleanWaterTime() {
+      waterTime = 0;
+  }
+  
+  public void stayInWater() {
+      waterTime++;
+  }
+  
+  public int getWaterTime() {
+      return waterTime;
+  }
 }
