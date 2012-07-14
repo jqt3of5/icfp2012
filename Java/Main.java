ee624e03c8ba56c764afea2fb4f8f088b0fc5032
chenli <lic9@uci.edu>
2012-07-14 16:09:28 -0700

    finished std input in Main

--- Java/Main.java
+++ Java/Main.java
@@ -4,20 +4,28 @@ import java.io.*;
 public class Main {
 
   public static void main(String[] args) {
+    
     // Add hook for
     Runtime.getRuntime().addShutdownHook(new Thread() {
-	@Override
-	public void run() {
-	  // TODO(jack): replace
-	  System.out.println("Shutdown hook ran!");
-	}
+  @Override
+  public void run() {
+    // TODO(jack): replace
+    System.out.println("Shutdown hook ran!");
+  }
       });
+
+    Scanner scan = new Scanner(System.in);
+    String map = "";
+    while(scan.hasNext()) {
+      map += scan.next() + "\n";
+    }
+    System.out.print(map);
 
     /*while (true) {
       try {
-	Thread.sleep(1000);
+  Thread.sleep(1000);
       } catch (InterruptedException e) {
-	System.out.println("interrupted: " + e);
+  System.out.println("interrupted: " + e);
       }
       }*/
 
