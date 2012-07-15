ffac586682ecd755e4d9fcdb9213e790d823efb6
Jack Cheng <skswbwt@gmail.com>
2012-07-15 01:44:55 -0700

    Cost functions.

--- Java/CostFunctions.java
+++ Java/CostFunctions.java
@@ -13,4 +13,18 @@ public class CostFunctions {
       return Math.abs(end.c-start.c) + Math.abs(end.r-start.r);
     }
   }
+
+  public static class BoardSensingCost implements Astar.CostFunction {
+    @Override
+    public double compute(final Board board, final Point start, final Point end) {
+      int r = end.r;
+      int c = end.c;
+
+      if (board.isDead) {
+	return Double.POSITIVE_INFINITY;
+      } else {
+	return board.robby.score;
+      }
+    }
+  }
 }
