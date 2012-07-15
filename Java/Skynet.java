383a20634d7ed9e90ac2df6f934cdf3d8e85c21e
Jack Cheng <skswbwt@gmail.com>
2012-07-15 15:51:45 -0700

    Temp Skynet

--- Java/Skynet.java
+++ Java/Skynet.java
@@ -1,9 +1,13 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Set;
 
 public abstract class Skynet {
   protected Board curBoard;
 
-  public Skynet(String mapStr) {
+  public Skynet(final String mapStr) {
     curBoard = new Board(mapStr);
   }
 
@@ -13,10 +17,40 @@ public abstract class Skynet {
     return curBoard.robby.getScore();
   }
 
-  public PriorityQueue<Point> findClosestLambdas(Point current) {
-    PriorityQueue<Point> retQueue = new PriorityQueue<Point>();
+  public Queue<Point> findClosestLambdas() {
+    final Queue<Point> retQueue = new LinkedList<Point>();
 
+    final Queue<BfsNode> bfsQ = new LinkedList<BfsNode>();
+    final Set<Point> visited = new HashSet<Point>();
+    bfsQ.add(new BfsNode(curBoard.robby.position, 0));
 
+    while (bfsQ.size() > 0) {
+      final BfsNode curNode = bfsQ.remove();
+      final Point curPoint = curNode.point;
+      if (visited.contains(curPoint)) continue;
+      visited.add(curPoint);
+
+      // add lambda points to queue
+      if (curBoard.map[curPoint.r][curPoint.c] == Board.CellTypes.Lambda) {
+        retQueue.add(curPoint);
+      }
+
+      // add children
+      final int[] dr = {-1, 0, 0, 1};
+      final int[] dc = {0, -1, 1, 0};
+
+      for (int i = 0; i < dr.length; i++) {
+        final int r = curPoint.r + dr[i];
+        final int c = curPoint.c + dc[i];
+
+        if (0 <= r && r < curBoard.height && 0 <= c && c < curBoard.width &&
+            curBoard.map[r][c] != Board.CellTypes.Wall) {
+          final Point childPoint = new Point(r, c);
+          if (! visited.contains(childPoint))
+            bfsQ.add(new BfsNode(childPoint, curNode.dist+1));
+        }
+      }
+    }
 
     return retQueue;
   }
@@ -31,19 +65,20 @@ public abstract class Skynet {
   public static abstract class AStarSkynet extends Skynet {
     AStar pathfinder;
     TerminationConditions.TerminationCondition terminator;
-    public AStarSkynet(String mapStr) {
+    public AStarSkynet(final String mapStr) {
       super(mapStr);
     }
   }
 
 
   public static class GreedySkynet extends AStarSkynet {
-    public GreedySkynet(String mapStr) {
+    public GreedySkynet(final String mapStr) {
       super(mapStr);
     }
 
+    @Override
     public String plan() {
-      Path totalPath = new Path();
+      final Path totalPath = new Path();
 
       while (curBoard.lambdaPos.size() > 0) {
         if (Main.gotSIGINT)
@@ -54,8 +89,8 @@ public abstract class Skynet {
         int bestLength = Integer.MAX_VALUE;
         Board bestBoard = curBoard;
         Path bestPath = null;
-        for (Point lambdaPt : curBoard.lambdaPos) {
-          Board newBoard = new Board(curBoard);
+        for (final Point lambdaPt : curBoard.lambdaPos) {
+          final Board newBoard = new Board(curBoard);
           terminator = new TerminationConditions.PointTermination(lambdaPt);
           pathfinder = new AStar(new CostFunctions.BoardSensingCost(),
                                  new CostFunctions.ManhattanCost(),
@@ -63,7 +98,7 @@ public abstract class Skynet {
           pathfinder.findPath(newBoard, lambdaPt);
           if (Main.gotSIGINT)
             return totalPath.toString();
-          Path path = terminator.getPath();
+          final Path path = terminator.getPath();
           if (path.size() < bestLength) {
             bestBoard = terminator.getBoard();
             bestPath = terminator.getPath();
@@ -74,7 +109,7 @@ public abstract class Skynet {
         totalPath.addAll(bestPath);
       }
 
-      Board newBoard = new Board(curBoard);
+      final Board newBoard = new Board(curBoard);
       terminator = new TerminationConditions.PointTermination(curBoard.liftLocation);
       pathfinder = new AStar(new CostFunctions.BoardSensingCost(), new CostFunctions.ManhattanCost(), terminator);
       pathfinder.findPath(newBoard, curBoard.liftLocation);
@@ -87,30 +122,35 @@ public abstract class Skynet {
     }
   }
 
-  public static class GreedierSkynet implements Skynet {
+  public static class GreedierSkynet extends Skynet {
+    public GreedierSkynet(String mapString) {
+      super(mapString);
+    }
+
     @Override
     public String plan() {
-
+      return null;
     }
   }
 
-/*
- * DOES NOT WORK YET!
- */
+  /*
+   * DOES NOT WORK YET!
+   */
   class AnnealingSkynet extends AStarSkynet {
     Random rand = new Random();
     static final int MAX_TIME = 1000;
 
-    public AnnealingSkynet(String mapStr) {
+    public AnnealingSkynet(final String mapStr) {
       super(mapStr);
     }
 
+    @Override
     public String plan() {
       double curEnergy = energy(curBoard);
       for (int time = 0; time < MAX_TIME; time++) {
-        double temperature = temperature((double)time/MAX_TIME);
-        Board newBoard = pickNeighbor();
-        double newEnergy = energy(newBoard);
+        final double temperature = temperature((double)time/MAX_TIME);
+        final Board newBoard = pickNeighbor();
+        final double newEnergy = energy(newBoard);
         if (evaluate(curEnergy, newEnergy, temperature) > rand.nextDouble()) {
           curBoard = newBoard;
           curEnergy = newEnergy;
@@ -120,26 +160,36 @@ public abstract class Skynet {
       return curBoard.toString();
     }
 
-    double energy(Board b) {
+    double energy(final Board b) {
       return 1.0;
     }
 
-    double temperature(double timeRatio) {
+    double temperature(final double timeRatio) {
       return 1-timeRatio;
     }
 
     Board pickNeighbor() {
-      int i = rand.nextInt(curBoard.lambdaPos.size());
+      final int i = rand.nextInt(curBoard.lambdaPos.size());
 
       pathfinder.findPath(curBoard, curBoard.lambdaPos.get(i));
-      Board newBoard = new Board(curBoard);
+      final Board newBoard = new Board(curBoard);
       //newBoard.move(path);
       return newBoard;
     }
 
-    double evaluate(double curEnergy, double newEnergy, double temperature) {
+    double evaluate(final double curEnergy, final double newEnergy, final double temperature) {
       return 1.0;
     }
   }
 
+
+  // -------------- Util things ----------------
+  private class BfsNode {
+    public Point point;
+    public double dist;
+    public BfsNode(final Point point, final double dist) {
+      this.point = point;
+      this.dist = dist;
+    }
+  }
 }
