dde714ca644ab023cb54063e09af353674865564
jt <jqt3of5>
2012-07-15 19:32:22 -0600

    skeleton hungry skynet

---
+++ Java/HungrySkynet.java
@@ -0,0 +1,34 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+
+public class HungrySkynet extends Skynet {
+
+  public interface Strategy
+  {
+    public int value();
+    
+  }
+  
+  PriorityQueue<Strategy> strategies;
+  
+  public HungrySkynet(String mapStr) {
+    super(mapStr);
+    strategies = new PriorityQueue<Strategy>(11, new Comparator<Strategy>() {
+
+      @Override
+      public int compare(Strategy arg0, Strategy arg1) {
+        return (arg0.value() - arg1.value() == 0 ? 0 : arg0.value()-arg1.value()) ;
+      } });
+  }
+
+  @Override
+  public String plan() {
+    
+    
+    return null;
+  }
+
+  
+  
+}
