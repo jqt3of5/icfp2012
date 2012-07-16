396e91538b3b635d34130ca725383071945baa83
jt <jqt3of5>
2012-07-15 20:10:28 -0600

    pretty simple implementation

--- Java/HungrySkynet.java
+++ Java/HungrySkynet.java
@@ -5,9 +5,31 @@ public class HungrySkynet extends Skynet {
 
   public interface Strategy {
     public int value();
-
+    public Path getPath();
+    public int evaluate(BoardState currentBoard);
   }
 
+  public class Shave implements Strategy
+  {
+    int value = -1;
+    Path stratPath = new Path();
+    @Override
+    public int value() {
+      return value;
+    }
+
+    @Override
+    public Path getPath() {
+      return stratPath;
+    }
+
+    @Override
+    public int evaluate(BoardState currentBoard) {
+ 
+      return 0;
+    }
+    
+  }
   PriorityQueue<Strategy> strategies;
 
   public HungrySkynet(String mapStr) {
